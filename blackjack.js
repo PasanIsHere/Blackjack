0ca7565cddc40d4108eed65c9ea71a3964cca477
Pasan <pasanu@protonmail.com>
2022-10-21 01:18:15 -0400

    Updated blackjack,js
    Can get value of a card, check if a card is an ace, and a basic start state for the game

--- blackjack.js
+++ blackjack.js
@@ -17,11 +17,24 @@ var dealerHidden; //hidden card of the dealer
 var canHit = true;//let's the player(you) hit while their sum <=21
 
 window.onload= function(){
-    buildDeck();
+    createDeck();
     shuffleDeck();
+    startGame();
+    console.log(hidden);
+    console.log(dealerSum);
+    while(dealerSum < 17){ //Dealer Game Logic if they have < 17 score they must draw.
+        let cardImg = document.createElement("img") //creating an image tage
+        let card = deck.pop();
+        cardImg.src = "./cards/"+ card +".png";
+        dealerSum += getValue(card);
+        dealerAceCount += checkAce(card);
+        document.getElementById("dealer-cards").append(cardImg);
+
+    }
+    console.log(dealerSum);
 }
 
-function buildDeck(){
+function createDeck(){
     let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
     let types = ["C", "D", "H", "S"]; //Type of Card: C = Club, D = Diamond. H = Hearts, S = Spade
     deck = [];
@@ -42,12 +55,37 @@ function buildDeck(){
 
 }
 
-
-
 function shuffleDeck() {
     for (let i = deck.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [deck[i], deck[j]] = [deck[j], deck[i]];
     }
     console.log(deck);
+}
+
+function startGame(){
+    hidden = deck.pop();
+    dealerSum += getValue(hidden);
+    
+    dealerAceCount  += checkAce(hidden);
+}
+
+function getValue(card){
+    let data = card.split("-") //card format is 'value-type', splitting by '-' , data[0] will hold value, data[1] will hold the type
+    let value = data[0];
+    
+    if (isNaN(value)){ // Aces, Jacks, Queens, and Kings
+        if (value == "Ace"){ //Ace
+            return 11;
+        }
+        return 10;
+    }
+    return parseInt(value);
+}
+
+function checkAce(card){
+    if (card[0]== "A"){ //'value-type' format, if first char is "A" it's an Ace
+        return 1;
+    }
+    return 0;
 }
